5f9610c0dab89ea29e143a730342ee678b32fd7e
Jakub Majer <jakub.majer11@gmail.com>
2026-02-10 21:37:01 +0100

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("clickBtn");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+  output.textContent = "Kliknul jsi mě! 👊";
+});
